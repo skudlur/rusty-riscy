73d88f1e6208d3bb0719b9dc628c1e44fbab3a9d
suhaskv1 <suhaskv0111@gmail.com>
2022-03-20 13:28:25 +0530

    added logo and cls to init

--- src/src/main.rs
+++ src/src/main.rs
@@ -4,6 +4,7 @@
 
 use std::io;
 use std::{thread, time};
+use std::fs;
 
 fn sleep() {
     let ten_millis = time::Duration::from_millis(1000);
@@ -13,8 +14,18 @@ fn sleep() {
     assert!(now.elapsed() >= ten_millis);
 }
 
+fn print_logo() {
+    print!("{}[2J", 27 as char); // to clear terminal screen
+
+    let filename = "logo.txt";
+    let logo_con = fs::read_to_string(filename)
+        .expect("Failed to read the file");
+
+    println!("{}",logo_con);
+}
+
 fn main() {
-    println!("rusty-riscy");
+    print_logo();
 
     loop {
         println!("Enter the option: ");
@@ -26,10 +37,11 @@ fn main() {
             .expect("Failed to read line");
 
         let trim_option = option.trim();
+        let mut trim_flag = false;
 
         match trim_option.parse::<u32>() {
             Ok(i) => println!("You entered option number: {}", i),
-            Err(..) => println!("This is not an integer: {}", trim_option),
+            Err(..) => println!("This is not an integer!"),
         };
 
         if true {
