cf576ac76edc6eacce7c1924aba2aae0fbaa9c8e
suhaskv <suhaskv0111@gmail.com>
2022-08-15 20:11:05 +0530

    src dir delete

--- src/target/debug/deps/src-5bdb0766478866ef.d
+++
@@ -1,5 +0,0 @@
-/home/suhas/rusty-riscy/src/target/debug/deps/src-5bdb0766478866ef: src/main.rs
-
-/home/suhas/rusty-riscy/src/target/debug/deps/src-5bdb0766478866ef.d: src/main.rs
-
-src/main.rs:
--- src/target/debug/src.d
+++
@@ -1,1 +0,0 @@
-/home/suhas/rusty-riscy/src/target/debug/src: /home/suhas/rusty-riscy/src/src/main.rs
