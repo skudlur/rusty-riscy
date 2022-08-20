73e45fcfd938a8c0354a0be54b0edba9133e11dc
suhaskv <suhaskv0111@gmail.com>
2022-08-20 19:04:40 +0530

    added sysinfo and some minor changes but wip

--- src/main.rs
+++ src/main.rs
@@ -26,10 +26,46 @@
 use std::io;
 use std::{thread, time};
 use std::fs;
+use sysinfo::{NetworkExt, NetworksExt, System, SystemExt};
 
 pub mod primetest;
 
+fn sys_info() {
+    /*
+        System information initial list
+    */
+
+    let mut sys = System::new_all();
+
+    let sys_name = sys.name();
+    let sliced_sys_name = &sys_name[0..4];
+
+    println!("System name:             {:?}", sliced_sys_name);
+    println!("System kernel version:   {:?}", sys.kernel_version());
+    println!("System OS version:       {:?}", sys.os_version());
+    println!("System host name:        {:?}", sys.host_name());
+
+    println!("NB CPUs: {}", sys.cpus().len());
+
+    println!("=> system:");
+    // RAM and swap information:
+    let mut mb_total_memory = 0;
+    let mut mb_used_memory = 0;
+    mb_total_memory = (sys.total_memory() / 1000) - 1125;
+    mb_used_memory = (sys.used_memory() / 1000) - 500; 
+    println!("total memory: {} MB", mb_total_memory);
+    println!("used memory : {} MB", mb_used_memory);
+
+    println!("=> disks:");
+    for disk in sys.disks() {
+        println!("{:?}", disk);
+    }
+}
+
 fn sleep() {
+
+    /* function to delay tests */
+
     let ten_millis = time::Duration::from_millis(1000);
     let now = time::Instant::now();
 
@@ -38,6 +74,9 @@ fn sleep() {
 }
 
 fn print_logo() {
+
+    /* function to print logo */
+
     print!("{}[2J", 27 as char); // to clear terminal screen
 
     let filename = "logo.txt";
@@ -48,8 +87,11 @@ fn print_logo() {
 }
 
 fn main() {
-    // main function that calls all initial functions
+    
+    /* main function that calls all initial functions */
+
     print_logo();
+    sys_info();
 
     loop {
         println!("Enter the option: ");
@@ -61,7 +103,7 @@ fn main() {
             .expect("Failed to read line");
 
         let trim_option = option.trim();
-        let mut trim_flag = false;
+        let trim_flag = false;
 
         match trim_option.parse::<u32>() {
             Ok(i) => println!("You entered option number: {}", i),
--- src/primetest.rs
+++ src/primetest.rs
@@ -1,16 +1,15 @@
-//
-// This program contains the prime number test to stress out all cores of the CPU.
-//
+/*
+ This program contains the prime number test to stress out all cores of the CPU.
+*/
 
-use std::io;
-use std::thread;
 use std::time::Instant;
+use std::ops::DivAssign;
 
 pub fn prime_stress_test() {
-    let mut num = 1;
+    let num = 1;
     let mut primes = 0;
 
-    let mut limit: i32 = 500000;
+    let limit: i32 = 500000;
 
     for num in 0..limit {
         let mut i = 2;
@@ -26,16 +25,24 @@ pub fn prime_stress_test() {
     }
 }
 
+struct Score { points: f64 }
+
+impl DivAssign<f64> for Score {
+    fn div_assign(&mut self, rhs: f64) {
+        self.points /= rhs;
+    }
+}
+
 pub fn main() {
-    println!("Prime number test!!");
-    let mut start = Instant::now();
+    println!("Prime number test!");
+    let start = Instant::now();
 
     prime_stress_test();
 
     let elapsed = start.elapsed();
     println!("Elapsed: {:.2?}", elapsed);
 
-    let mut score = 0;
-    let mut elapsed_int = 0;
+    let mut score = Score { points: 1000.0 };
+    let elapsed_int = 0;
 
 }
